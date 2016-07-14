061135ad4076b0e4c45d5ef8de3879ad425055a3
M42 <mromang08@gmail.com>
2016-07-14 19:21:32 +0200

    Sinatra - Initial commit

---
+++ form.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+get '/' do
+  haml :index
+end
+
+
+post '/' do
+  title = params[:title]
+  author = params[:author]
+  link = params[:link]
+
+  haml :index
+end
