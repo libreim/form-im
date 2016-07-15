2f032c5cb5dd5efec7d6ab5c8e9e86a35927c451
M42 <mromang08@gmail.com>
2016-07-15 12:39:20 +0200

    Octokit authentication First draft

--- form.rb
+++ form.rb
@@ -12,5 +12,32 @@ post '/' do
   author = params[:author]
   link = params[:link]
 
+  ## OCTOKIT
+  # Provide authentication credentials
+  repo = 'dgiim/blog'
+  ref = 'heads/master'
+  client = Octokit::Client.new :login => 'dgiimbot', :password => 'password'
+  user = client.user
+  user.login
+  
+
+  # Create new commit
+  create_commit(
+    "dgiim/blog",
+    "[form] Post: $title",
+    "????", # the SHA of the tree object the new commit will point to
+    "????", # parent
+  )
+    
+  # Pull request
+  client.create_pull_request(
+    "dgiim/blog",
+    "gh-pages",
+    "post-title-branch", ## TODO!
+    "Pull request title",
+    "Pull request body" ## optional
+  )
+  
+  
   haml :index
 end
