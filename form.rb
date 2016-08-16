a197cb5c36f62b488d0d291a89639510db88acf6
libreimbot <libreim.blog@gmail.com>
2016-08-16 17:58:39 +0200

    Working test script

--- form.rb
+++ form.rb
@@ -22,48 +22,25 @@ post '/' do
   repo = "libreim/herramientas-im"
   base = "master" # "gh-pages" for blog and awesome
   ref_base = "heads/#{base}"
-  head = "libreimbot:new-post"
+  head = "libreimbot-new-post"
   ref_head = "heads/#{head}"
 
-  g = Git.clone("https://github.com/#{repo}.git", "forms")
-  g.branch(head)
-  g.checkout(head)
-  g.chdir do
-    new_file("hey-test", "HEY")
-  end
+  dir = ".herramientas"
+
+  # Basically, there's no way for git to work directly with an access token,
+  # so GitHub accepts it as an username
+  # http://stackoverflow.com/a/24558935/5306389
+  g = Git.clone("https://#{ENV["LIBREIMBOT_TOKEN"]}@github.com/#{repo}.git", dir)
+
+  g.config('user.name', "libreimbot")
+  g.config('user.email', 'libreim.blog@gmail.com')
+  g.branch(head).checkout
+  File.write("#{dir}/hey-test", "HEYA")
   g.add(all: true)
   g.commit("This is a test!")
-  # g.push
-  sha_commit = github.ref(repo, ref_head).object.sha
-  puts sha_commit
-  github.update_ref(repo, ref_head, sha_commit)
+  g.push("origin", head)
 
   github.create_pull_request(repo, base, head, "This is an automated pull request!", "This is the body of an **automated** pull request. Exciting, isn't it?")
-
-  # now what?
-
-  # ref = 'heads/master'
-  # sha_latest_commit = github.ref(repo,ref).object.sha
-  # sha_base_tree = github.commit(repo, sha_latest_commit).commit.tree.sha
-  #
-  # # Filename
-  # file_name = File.join("some_dir","testfile")
-  # blob_sha = github.create_blob(repo,Base64.encode64(my_content),"base64")
-  # sha_new_tree = github.create_tree(repo,
-  #                                   [ { :path => file_name,
-  #                                       :mode => "100644",
-  #                                       :type => "blob",
-  #                                       :sha  => blob_sha } ],
-  #                                   { :base_tree => sha_base_tree }).sha
-  # # Create new commit
-  # commit_message = "Commited via Octokit!"
-  # sha_new_commit = github.create_commit(repo,
-  #                                       commit_message,
-  #                                       sha_new_tree,
-  #                                       sha_last_commit).sha
-  # updated_ref = github.update_ref(repo,ref,sha_new_commit)
-  # puts.updated_ref
-
 
   haml :index
 end
