b05923da0e9c5b48ac1bfb6be7700886a7b6e99f
David Charte <fdavidcl@protonmail.com>
2016-08-16 19:34:02 +0200

    Working new resource form

--- form.rb
+++ form.rb
@@ -4,78 +4,67 @@ require 'sinatra'
 require 'haml'
 require 'octokit'
 require 'git'
+require 'securerandom'
+require 'fileutils'
+
+# Provide authentication credentials
+github = Octokit::Client.new(:access_token => ENV["LIBREIMBOT_TOKEN"])
 
 get "/" do
   haml :index
 end
 
-post "/" do
+post "/resource" do
+  # Get parameters from request
   title = params[:title]
   author = params[:author]
   link = params[:link]
+  # section = params[:section]
+  # category = params[:category]
 
-  ## OCTOKIT
-  # Provide authentication credentials
-  github = Octokit::Client.new(:access_token => ENV["LIBREIMBOT_TOKEN"])
-  #repo = "m42/form-im"
-  repo = "libreim/herramientas-im"
-  base = "master" # "gh-pages" for blog and awesome
+  repo = "libreim/awesome"
+  base = "gh-pages"
   ref_base = "heads/#{base}"
-  head = "libreimbot-new-post"
+  head = "new-resource-#{SecureRandom.uuid}"
   ref_head = "heads/#{head}"
 
-  dir = ".herramientas"
+  dir = "awesome"
 
   # Basically, there's no way for git to work directly with an access token,
   # so GitHub accepts it as an username
   # http://stackoverflow.com/a/24558935/5306389
-  g = Git.clone("https://#{ENV["LIBREIMBOT_TOKEN"]}@github.com/#{repo}.git", dir)
+  begin
+    g = Git.clone("https://#{ENV["LIBREIMBOT_TOKEN"]}@github.com/#{repo}.git", dir)
 
-  g.config('user.name', "libreimbot")
-  g.config('user.email', 'libreim.blog@gmail.com')
-  g.branch(head).checkout
-  File.write("#{dir}/hey-test", "HEYA")
-  g.add(all: true)
-  g.commit("This is a test!")
-  g.push("origin", head)
+    # Use the bot's name and email
+    g.config("user.name", "libreimbot")
+    g.config("user.email", 'libreim.blog@gmail.com')
+    # Create the new branch
+    g.branch(head).checkout
 
-  github.create_pull_request(repo, base, head, "This is an automated pull request!", "This is the body of an **automated** pull request. Exciting, isn't it?")
+    g.chdir do
+      # Add resource to unclassified
+      File.open("_common/unclassified.md", "a") do |file|
+        file.puts(if link.empty?
+          "**#{title}** - #{author}  "
+        else
+          "[#{title} - #{author}](#{link})  "
+        end)
+      end
+    end
 
-  haml :index
-end
+    # Commit and push to new branch
+    g.add(all: true)
+    g.commit("Nuevo recurso: #{title}")
+    g.push("origin", head)
 
-post "/link" do
-  title = params[:title]
-  author = params[:author]
-  link = params[:link]
+    # Finally, create a pull request using octokit
+    github.create_pull_request(repo, base, head, "Nuevo recurso: #{title}", "Añade *[#{title}](#{link})* de #{author}.")
+  rescue StandardError => e
+  end
+  FileUtils.rm_rf(dir)
 
-
-  ## OCTOKIT
-  # Provide authentication credentials
-  github = Octokit::Client.new(:access_token => ENV["LIBREIMBOT_TOKEN"])
-  #repo = "m42/form-im"
-  repo = "libreim/herramientas-im"
-  base = "master" # "gh-pages" for blog and awesome
-  ref_base = "heads/#{base}"
-  head = "libreimbot-new-post"
-  ref_head = "heads/#{head}"
-
-  dir = ".herramientas"
-
-  # Basically, there's no way for git to work directly with an access token,
-  # so GitHub accepts it as an username
-  # http://stackoverflow.com/a/24558935/5306389
-  g = Git.clone("https://#{ENV["LIBREIMBOT_TOKEN"]}@github.com/#{repo}.git", dir)
-
-  g.config('user.name', "libreimbot")
-  g.config('user.email', 'libreim.blog@gmail.com')
-  g.branch(head).checkout
-  File.write("#{dir}/hey-test", "HEYA")
-  g.add(all: true)
-  g.commit("This is a test!")
-  g.push("origin", head)
-
-  github.create_pull_request(repo, base, head, "This is an automated pull request!", "This is the body of an **automated** pull request. Exciting, isn't it?")
-
+  # Return the user to the home page
+  # TODO: show success status (and link to new pull request)
   redirect to("/")
 end
